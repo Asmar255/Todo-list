9221e24e1aabaa239775d715990bf40679170a80
Asmar255 <asmarnaeem255@gmail.com>
2026-04-28 22:39:49 +0500

    made the todo list reponsive

--- todo list/src/App.jsx
+++ todo list/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
+import { MdModeEditOutline } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 
 function App() {
 
@@ -77,12 +79,13 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='container mx-auto my-3 rounded-lg bg-blue-300 py-4 px-3 max-w-4xl min-h-[83vh]'>
+      <div className='md:container md:mx-auto my-3 rounded-lg bg-blue-300 py-4 px-3 max-w-4xl min-h-[83vh] md:w-1/2'>
+        <h1 className='font-bold text-2xl text-center'>iTask - Your todo Taker</h1>
 
-        <div className="addtodo my-2">
+        <div className="addtodo my-2 flex flex-col gap-2">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" className='bg-white rounded-sm w-1/2' onChange={handleChnage} value={todo} />
-          <button onClick={handleAdd} disabled={todo.length<=3} className='cursor-pointer bg-blue-400 hover:bg-blue-500 disabled:bg-blue-600 p-3 py-1 rounded-md text-white text-sm font-bold mx-3'>Save</button>
+          <input type="text" className='bg-white rounded-sm w-full py-1' onChange={handleChnage} value={todo} />
+          <button onClick={handleAdd} disabled={todo.length<=3} className='cursor-pointer bg-blue-400 hover:bg-blue-500 disabled:bg-blue-600 p-3 py-1 rounded-md text-white text-sm font-bold mx-1'>Save</button>
         </div>
           
           <input  onChange={toggleFinished} type="checkbox" checked={showFinished} id="" /> Show Finished
@@ -92,16 +95,17 @@ function App() {
           {todos.length === 0 && <div className='text-xl text-gray-700 my-3'>No Todos to display</div>}
 
           {todos.map(item => {
-            return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex justify-between w-1/2 py-1">
-              <div className='flex gap-7'>
+            return (showFinished || !item.isCompleted) && 
+            <div key={item.id} className="todo flex justify-between w-auto py-1 md:w-1/2">
+              <div className='flex gap-7 '>
                 <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id="" />
                 <div className={`${item.isCompleted ? "line-through" : ""} text-lg`}>
                   {item.todo}
                 </div>
               </div>
               <div className="buttons flex h-full">
-                <button onClick={(e) => handleEdit(e, item.id)} className='cursor-pointer bg-blue-400 hover:bg-blue-500 p-3 py-1 rounded-md text-white text-sm font-bold mx-1'>Edit</button>
-                <button onClick={(e) => { handleDelete(e, item.id) }} className='cursor-pointer bg-blue-400 hover:bg-blue-500 p-3 py-1 rounded-md text-white text-sm font-bold mx-1'>Delete</button>
+                <button onClick={(e) => handleEdit(e, item.id)} className='cursor-pointer bg-blue-400 hover:bg-blue-500 p-3 py-1 rounded-md text-white text-sm font-bold mx-1'><MdModeEditOutline /></button>
+                <button onClick={(e) => { handleDelete(e, item.id) }} className='cursor-pointer bg-blue-400 hover:bg-blue-500 p-3 py-1 rounded-md text-white text-sm font-bold mx-1'><MdDelete /></button>
               </div>
             </div>
           })}
